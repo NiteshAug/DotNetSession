b0e0273cd3db9455c320bc996f096f53708e7194
DESKTOP-KO7FJ3U\NiteshChaudhary <nitesh.chaudhary@augmentolabs.com>
2022-07-26 17:34:57 +0530

    Added ClassLibrary Project

---
+++ ClassLibraryProject/ClassLibrary/Demo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Demo
+    {
+        public void message()
+        {
+            Console.WriteLine("I am great");
+        }
+    }
+
+    public static class Display 
+    {
+        public static void displayMessage()
+        {
+            Console.WriteLine("I am Great");
+        }
+    }
+}
---
+++ ClassLibraryProject/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibrary;
+using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Demo demo = new Demo();
+            demo.message();
+
+            //Static Class with Static Method
+            Display.displayMessage();
+        }
+    }
+}
