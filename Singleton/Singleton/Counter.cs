537614255ce921f2941dbd158a5aeaa6e481de62
DESKTOP-KO7FJ3U\NiteshChaudhary <nitesh.chaudhary@augmentolabs.com>
2022-07-26 18:20:48 +0530

    Added Singleton Project

---
+++ Singleton/Singleton/Counter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Counter
+    {
+        static int count = 0;
+        private static Counter getInstance = null;
+        public Counter()
+        {
+            count++;
+            Console.WriteLine("Instances created " + count);
+        }
+
+        public static Counter GetInstance
+        {
+            get
+            {
+                if (getInstance == null)
+                {
+                    getInstance = new Counter();
+                }
+                return getInstance;
+            }
+        }
+
+        public int Increment()
+        {
+            Console.WriteLine(count++);
+            return count++;
+        }
+
+        public int Decrement()
+        {
+            Console.WriteLine(count--);
+            return count--;
+        }
+    }
+}
---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Counter.GetInstance.Increment();
+            Counter.GetInstance.Increment();
+            Counter.GetInstance.Increment();
+
+            Console.WriteLine("Decrement Started");
+
+            Counter.GetInstance.Decrement();
+            Counter.GetInstance.Decrement();
+            Counter.GetInstance.Decrement();
+            Console.ReadLine();
+        }
+    }
+}
