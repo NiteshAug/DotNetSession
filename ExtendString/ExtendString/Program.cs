23e1bacd616c142d4ad3aa8e531c0bf335cd3eb4
DESKTOP-KO7FJ3U\NiteshChaudhary <nitesh.chaudhary@augmentolabs.com>
2022-07-26 18:59:31 +0530

    Added ExtendedString Project

---
+++ ExtendString/ExtendString/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ExtendString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pg =new Program();
+            Console.WriteLine("Enter Value");
+            string Value = Console.ReadLine();            
+            Console.WriteLine(pg.ToCurrency(Value));
+        }
+
+        public string ToCurrency(string value)
+        {
+            bool result = value.All(char.IsDigit);
+            if (result)
+            {
+                return "$" + value;
+            } else
+            {
+                return value;
+            }
+            
+        }
+    }
+}
