88106dda9d5b0199651ae6d44d977faea5cb1b1f
DESKTOP-KO7FJ3U\NiteshChaudhary <nitesh.chaudhary@augmentolabs.com>
2022-08-02 16:59:48 +0530

    Delegated Design Change

--- DelegatesPractice/DelegatesPractice/DelegatesImpl.cs
+++ DelegatesPractice/DelegatesPractice/DelegatesImpl.cs
@@ -10,7 +10,7 @@ namespace DelegatesPractice
     {
         public delegate void DelegatesPrac(string value);
 
-        public static void onWord(string value)
+        /*public static void onWord(string value)
         {
             Console.WriteLine("Calling onWord Delegate. Your input is " + value);
         }
@@ -23,7 +23,7 @@ namespace DelegatesPractice
         public static void onJunk(string value)
         {
             Console.WriteLine("Calling onJunk Delegate. Your input is " + value);
-        }
+        }*/
 
         public static void DelegateCallBack(string value, DelegatesPrac delegatesPrac)
         {
@@ -32,22 +32,89 @@ namespace DelegatesPractice
 
         public static void ReadingValue(string inputFromConsole)
         {
-            DelegatesPrac delegatesPrac = null;
+            InputTypeFactory inputTypeFactory = new InputTypeFactory();
+            IInputType inputType = inputTypeFactory.createInputType(inputFromConsole);
+            inputType.printInputType(inputFromConsole);
 
-            if (inputFromConsole.All(char.IsDigit) == true)
+            //DelegatesPrac delegatesPrac = null;
+
+            /*if (inputFromConsole.All(char.IsDigit) == true)
             {
-                delegatesPrac += onNumber;
-                delegatesPrac.Invoke(inputFromConsole);
+                DelegatesImpl.onNumber(inputFromConsole);
+                *//*delegatesPrac += onNumber;
+                delegatesPrac.Invoke(inputFromConsole);*//*
             }
             else if (Regex.IsMatch(inputFromConsole, "^[a-zA-Z0-9]*$"))
             {
-                delegatesPrac += onWord;
-                delegatesPrac.Invoke(inputFromConsole);
+                DelegatesImpl.onWord(inputFromConsole);
+                *//*delegatesPrac += onWord;
+                delegatesPrac.Invoke(inputFromConsole);*//*
             }
             else if (!Regex.IsMatch(inputFromConsole, "^[a-zA-Z0-9]*$"))
             {
-                delegatesPrac += onJunk;
-                delegatesPrac.Invoke(inputFromConsole);
+                DelegatesImpl.onJunk(inputFromConsole);
+                *//*delegatesPrac += onJunk;
+                delegatesPrac.Invoke(inputFromConsole);*//*
+            }*/
+        }
+
+
+
+        public interface IInputType
+        {
+            void printInputType(string input);
+        }
+
+
+        public class WordInputType : IInputType
+        {
+            public void printInputType(string input)
+            {
+                Console.WriteLine("Calling onWord Delegate. Your input is " + input);
+            }
+
+        }
+
+        public class NumberInputType : IInputType
+        {
+            public void printInputType(string input)
+            {
+                Console.WriteLine("Calling onNumber Delegate. Your input is " + input);
+            }
+
+        }
+
+        public class JunkInputType : IInputType
+        {
+            public void printInputType(string input)
+            {
+                Console.WriteLine("Calling onJunk Delegate. Your input is " + input);
+            }
+
+        }
+
+
+        public class InputTypeFactory
+        {
+            public IInputType createInputType(string inputType)
+            {
+
+                if (inputType.All(char.IsDigit) == true)
+                {
+                    return new NumberInputType();
+                }
+                else if (Regex.IsMatch(inputType, "^[a-zA-Z0-9]*$"))
+                {
+                    return new WordInputType();
+                }
+                else if (!Regex.IsMatch(inputType, "^[a-zA-Z0-9]*$"))
+                {
+                    return new JunkInputType();
+                }
+                else
+                {
+                    return null;
+                }
             }
         }
     }
