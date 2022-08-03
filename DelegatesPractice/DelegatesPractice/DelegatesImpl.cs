7b54b38302c7223dee6f9dafe09aa564875079f8
DESKTOP-KO7FJ3U\NiteshChaudhary <nitesh.chaudhary@augmentolabs.com>
2022-08-03 12:45:17 +0530

    Added WebApplication Print hello world

--- DelegatesPractice/DelegatesPractice/DelegatesImpl.cs
+++ DelegatesPractice/DelegatesPractice/DelegatesImpl.cs
@@ -10,7 +10,7 @@ namespace DelegatesPractice
     {
         public delegate void DelegatesPrac(string value);
 
-        /*public static void onWord(string value)
+        public static void onWord(string value)
         {
             Console.WriteLine("Calling onWord Delegate. Your input is " + value);
         }
@@ -23,98 +23,33 @@ namespace DelegatesPractice
         public static void onJunk(string value)
         {
             Console.WriteLine("Calling onJunk Delegate. Your input is " + value);
-        }*/
+        }
 
         public static void DelegateCallBack(string value, DelegatesPrac delegatesPrac)
         {
+            
             delegatesPrac(value);
         }
 
         public static void ReadingValue(string inputFromConsole)
         {
-            InputTypeFactory inputTypeFactory = new InputTypeFactory();
-            IInputType inputType = inputTypeFactory.createInputType(inputFromConsole);
-            inputType.printInputType(inputFromConsole);
 
-            //DelegatesPrac delegatesPrac = null;
+           DelegatesPrac delegatesPrac = null;
 
-            /*if (inputFromConsole.All(char.IsDigit) == true)
+            if (inputFromConsole.All(char.IsDigit) == true)
             {
-                DelegatesImpl.onNumber(inputFromConsole);
-                *//*delegatesPrac += onNumber;
-                delegatesPrac.Invoke(inputFromConsole);*//*
+                delegatesPrac += onNumber;
+                delegatesPrac.Invoke(inputFromConsole);
             }
             else if (Regex.IsMatch(inputFromConsole, "^[a-zA-Z0-9]*$"))
             {
-                DelegatesImpl.onWord(inputFromConsole);
-                *//*delegatesPrac += onWord;
-                delegatesPrac.Invoke(inputFromConsole);*//*
+                delegatesPrac += onWord;
+                delegatesPrac.Invoke(inputFromConsole);
             }
             else if (!Regex.IsMatch(inputFromConsole, "^[a-zA-Z0-9]*$"))
             {
-                DelegatesImpl.onJunk(inputFromConsole);
-                *//*delegatesPrac += onJunk;
-                delegatesPrac.Invoke(inputFromConsole);*//*
-            }*/
-        }
-
-
-
-        public interface IInputType
-        {
-            void printInputType(string input);
-        }
-
-
-        public class WordInputType : IInputType
-        {
-            public void printInputType(string input)
-            {
-                Console.WriteLine("Calling onWord Delegate. Your input is " + input);
-            }
-
-        }
-
-        public class NumberInputType : IInputType
-        {
-            public void printInputType(string input)
-            {
-                Console.WriteLine("Calling onNumber Delegate. Your input is " + input);
-            }
-
-        }
-
-        public class JunkInputType : IInputType
-        {
-            public void printInputType(string input)
-            {
-                Console.WriteLine("Calling onJunk Delegate. Your input is " + input);
-            }
-
-        }
-
-
-        public class InputTypeFactory
-        {
-            public IInputType createInputType(string inputType)
-            {
-
-                if (inputType.All(char.IsDigit) == true)
-                {
-                    return new NumberInputType();
-                }
-                else if (Regex.IsMatch(inputType, "^[a-zA-Z0-9]*$"))
-                {
-                    return new WordInputType();
-                }
-                else if (!Regex.IsMatch(inputType, "^[a-zA-Z0-9]*$"))
-                {
-                    return new JunkInputType();
-                }
-                else
-                {
-                    return null;
-                }
+                delegatesPrac += onJunk;
+                delegatesPrac.Invoke(inputFromConsole);
             }
         }
     }
---
+++ WebApplication/WebApplication/Controllers/Hello.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Hello : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult hello()
+        {
+            string result = "Hello World";
+            return Ok(result);
+        }
+    }
+}
