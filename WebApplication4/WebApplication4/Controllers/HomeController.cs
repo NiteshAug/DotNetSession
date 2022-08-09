5b93c8e8ae1fbd1b25ed96b9109c3848028c1fd0
DESKTOP-KO7FJ3U\NiteshChaudhary <nitesh.chaudhary@augmentolabs.com>
2022-08-09 17:58:26 +0530

    Added Webapplication 4 (ASP.NET 4) Log the parameter sent

---
+++ WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebApplication4.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class HomeController : Controller
+    {
+        private List<string> _users = new List<string>();
+        [HttpGet]
+        public IEnumerable<string> Get(string value)
+        {
+            _users.Add(value);
+
+            return _users;
+        }
+    }
+}
---
+++ WebApplication4/WebApplication4/RequestLogging/RequestLoggingFac.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.IO;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WebApplication4.RequestLogging
+{
+    public class RequestLoggingFac
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+        private readonly RecyclableMemoryStreamManager _recyclableMemoryStreamManager;
+
+        public RequestLoggingFac(RequestDelegate next, ILoggerFactory loggerFactory)
+        {
+            _next = next;
+            _logger = loggerFactory.CreateLogger<RequestLoggingFac>();
+            _recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await LogRequest(context);
+        }
+
+        private async Task LogRequest(HttpContext context)
+        {
+            context.Request.EnableBuffering();
+
+            await using var requestStream = _recyclableMemoryStreamManager.GetStream();
+            await context.Request.Body.CopyToAsync(requestStream);
+            _logger.LogInformation($"Http Request Information:{Environment.NewLine}" +
+                                   $"Schema:{context.Request.Scheme} " +                                   
+                                   $"QueryString: {context.Request.QueryString} ");
+            context.Request.Body.Position = 0;
+        }
+    }
+}
